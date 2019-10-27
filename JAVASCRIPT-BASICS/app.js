b54f0833629d7b4cc1190e8763c23f2164a7179f
ArturLinnik <arturlin2018@gmail.com>
2019-10-27 14:06:36 +0100

    Finished learning the bases of Javascript

--- JAVASCRIPT-BASICS/app.js
+++ JAVASCRIPT-BASICS/app.js
@@ -1,20 +1,64 @@
 
-document.write("<h1>Hello World</h1>");
+// Variables 
 
-// Object
+var username = "John";
+let lastname = "Carter" // let and var are nearly the same
 
-console.log({
-    "username": 'Ryan',
-    "score": 70.4,
-    "hours": 14,
-    "professional": true,
-    "friends":['Artur', 'Alejandro', 'Jonathan']
-})
+const PI = 3.1415; // constant
 
-// {
-//     "username": 'Ryan',
-//     "score": 70.4,
-//     "hours": 14,
-//     "professional": true,
-//     "friends":['Artur', 'Alejandro', 'Jonathan']
-// }
+console.log(PI)
+
+// If-else statement
+
+let passwordDB = "pepe123"
+let input = "asdasdasdasd"
+
+let result = input == passwordDB;
+
+
+if (result == true){
+    console.log("Correct login");
+}
+else {
+    console.log("Incorrect password");
+}
+
+// Switch statement
+
+let typeCard = "Nothing"
+
+switch(typeCard){
+    case "Debit Card":
+        console.log("This is a Debit Card");
+        break;
+    case "Credit Card":
+        console.log("This is a Credit Card");
+        break;
+    default:
+        console.log("No card");
+}
+
+// While loop
+
+let count = 50;
+
+while(count > 0){
+    console.log("Hello World");
+    count--;
+}
+
+// For loop
+
+let names = ["Ryan", "Joe", "John"];
+
+for(let i = 0; i < names.length; i++){
+    console.log(names[i]);
+}
+
+// Functions 
+
+function greeting(name) {
+    console.log("Hello " + name);
+}
+
+greeting("Artur");
