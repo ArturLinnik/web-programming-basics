46c73b743fd74759b75183a8e2ebbc872d911675
ArturLinnik <arturlin2018@gmail.com>
2019-10-27 03:57:42 +0100

    Writing on webpage, objects and printing in console

---
+++ JAVASCRIPT-BASICS/app.js
@@ -0,0 +1,20 @@
+
+document.write("<h1>Hello World</h1>");
+
+// Object
+
+console.log({
+    "username": 'Ryan',
+    "score": 70.4,
+    "hours": 14,
+    "professional": true,
+    "friends":['Artur', 'Alejandro', 'Jonathan']
+})
+
+// {
+//     "username": 'Ryan',
+//     "score": 70.4,
+//     "hours": 14,
+//     "professional": true,
+//     "friends":['Artur', 'Alejandro', 'Jonathan']
+// }
